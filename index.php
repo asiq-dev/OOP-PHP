872ff5bdd9173f12a2811ac6d402e88afe8e1db9
asiqul islam <asiq.dev@gmail.com>
2022-02-15 23:18:32 +0600

    constant properties

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
 <?php
 
-class man{
-	public $name;
-	public $age;
-	public $height;
+class bubt{
+	const cse = "Welcome to Cse Department";
 	
-
-    function __construct($name,$age,$height){
-	$this->name = $name;
-	$this->age = $age;
-	$this->height = $height;
-	}	
-    protected function info(){
-	  return "Name: ".$this->name."age: ".$this->age."height: ".$this->height;
+	function showDept(){
+		return self :: cse;
 	}
+
 }
-class monjur extends man{
-	public $address;
-	 function __construct($name,$age,$height,$address){
-		$this->address = $address;
-		$this->name = $name;
-	    $this->age = $age;
-	    $this->height = $height;
-	 }
-	public function message(){
-	return $this->name.$this->age.$this->height.$this->address;
-	}
-}
+//echo bubt :: cse;
+//without object we can access properties and show
 
-$monjur = new monjur("monjur",16,5.4,"Noakhali");
-
-echo $monjur->message();
-
+$obj = new bubt();
+echo $obj->showDept();
+  
 
 ?>
 
