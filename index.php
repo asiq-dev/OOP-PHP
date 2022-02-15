20b22e497acecb3bb571c4fdc1d6970fc8310289
asiqul islam <asiq.dev@gmail.com>
2022-02-15 14:18:53 +0600

    inherite main class with child class properties

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 
 class man{
 	public $name;
-	public $age = 23;
-	public $height= 5.6;
+	public $age;
+	public $height;
+	
 
     function __construct($name,$age,$height){
 	$this->name = $name;
@@ -15,12 +16,19 @@ class man{
 	}
 }
 class monjur extends man{
+	public $address;
+	 function __construct($name,$age,$height,$address){
+		$this->address = $address;
+		$this->name = $name;
+	    $this->age = $age;
+	    $this->height = $height;
+	 }
 	public function message(){
-	return $this->info();
+	return $this->name.$this->age.$this->height.$this->address;
 	}
 }
 
-$monjur = new monjur("monjur",16,5.4);
+$monjur = new monjur("monjur",16,5.4,"Noakhali");
 
 echo $monjur->message();
 
